bf92a9f9d23e8c2c80676b462c6a2edd37bd7a4e
iwannadevops <dudwls3320@gamil.com>
2019-09-05 20:47:43 +0900

    :art:re-define seqquelize

--- models/tables/index.js
+++ models/tables/index.js
@@ -8,10 +8,12 @@ const env = process.env.NODE_ENV || 'development';
 // const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-const sequelize = new Sequelize('triJournal', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 const User = require('./User')(sequelize, Sequelize);
 const Journal = require('./Journal')(sequelize, Sequelize);
